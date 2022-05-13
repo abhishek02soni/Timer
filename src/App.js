2c01b40f0ca9f64cac782d3126e8921766254756
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-05-13 17:15:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./styles.css";
-
+import Timer from "./components/Timer";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Timer/>
     </div>
   );
 }
