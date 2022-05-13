e37f727cf398ee6929e844de2a804f0fe59dbd45
abhishek02soni <92570611+abhishek02soni@users.noreply.github.com>
2022-05-13 17:08:50 +0530

    Initial commit

---
+++ src/comopoents/Timer.jsx
@@ -0,0 +1,36 @@
+import React,{useEffect,useState} from "react";
+export default function Timer(){
+  const [seconds,setSeconds] = useState(0);
+  const [minutes,setMinutes] = useState(0);
+  
+ let timer;
+  useEffect(()=>{
+   timer = setInterval(()=>{
+      setSeconds(seconds+1);
+      if(seconds === 59){
+        setMinutes(minutes+1);
+        setSeconds(0);
+      }
+    },1000)
+    return () => clearInterval(timer);
+  });
+  const restart=()=>{
+   setSeconds(0);
+   setMinutes(0);
+  }
+  const stop = () =>{
+  clearInterval(timer);
+  }
+  return(
+    <div className="timer">
+     <div className="container">
+       <div className="timer_container">
+         <h1>Timer</h1>
+         <h1>{minutes<10?'0'+minutes:minutes}:{seconds<10?'0'+seconds:seconds}</h1>
+        <button className="restart" onClick={restart}>restart</button>
+        <button className="stop" onClick={stop}>stop</button>
+       </div>
+     </div>
+    </div>
+  )
+}
